736e82a3b500f5cee15b9f4742ce8311125b3d1a
Chayanon Ninyawee <cninyawe@z2t11c2.42bangkok.com>
2026-05-25 20:32:03 +0700

    Re-implementing the file reader so that it linked each char buffer instead each char individualy

--- srcs/file_reader.c
+++ srcs/file_reader.c
@@ -6,7 +6,7 @@
 /*   By: cninyawe <cninyawe@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 20:02:41 by cninyawe          #+#    #+#             */
-/*   Updated: 2026/05/25 20:05:29 by cninyawe         ###   ########.fr       */
+/*   Updated: 2026/05/25 20:30:28 by cninyawe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,48 +21,61 @@ static t_file_data	invalid_file_data(void)
 	return ((t_file_data){.data = NULL, .size = 0});
 }
 
-static char	*list_to_char_array(t_linked_list list)
+static int	append_buffer(t_linked_list *list, char *buf, int size)
+{
+	char	*chunk;
+	int		i;
+
+	chunk = malloc(sizeof(char) * size);
+	if (!chunk)
+		return (0);
+	i = 0;
+	while (i < size)
+	{
+		chunk[i] = buf[i];
+		i++;
+	}
+	if (!append_linked_list(list, chunk))
+		return (free(chunk), 0);
+	return (1);
+}
+
+static char	*list_to_char_array(t_linked_list list, int total_size)
 {
 	char		*arr;
 	t_list_node	*node;
+	char		*chunk;
 	int			i;
+	int			j;
 
-	arr = malloc(sizeof(char) * list.size);
+	arr = malloc(sizeof(char) * total_size);
 	if (!arr)
 		return (NULL);
 	node = list.head;
 	i = 0;
 	while (node)
 	{
-		arr[i++] = *(char *)node->data;
+		chunk = node->data;
+		j = 0;
+		while (j < BUFFER_SIZE && i < total_size)
+			arr[i++] = chunk[j++];
 		node = node->next;
 	}
 	return (arr);
 }
 
-static int	open_file(char *path, int *fd)
+static int	read_to_list(int fd, t_linked_list *list, int *total_size)
 {
-	*fd = open(path, O_RDONLY);
-	if (*fd < 0)
-		return (0);
-	return (1);
-}
+	int		read_size;
+	char	buf[BUFFER_SIZE];
 
-static int	append_buffer(t_linked_list *list, char *buf, int size)
-{
-	char	*c;
-	int		i;
-
-	i = 0;
-	while (i < size)
+	read_size = read(fd, buf, BUFFER_SIZE);
+	while (read_size > 0)
 	{
-		c = malloc(sizeof(char));
-		if (!c)
+		if (!append_buffer(list, buf, read_size))
 			return (0);
-		*c = buf[i];
-		if (!append_linked_list(list, c))
-			return (free(c), 0);
-		i++;
+		*total_size += read_size;
+		read_size = read(fd, buf, BUFFER_SIZE);
 	}
 	return (1);
 }
@@ -72,23 +85,18 @@ t_file_data	read_file_all(char *path)
 	t_linked_list	list;
 	t_file_data		file_data;
 	int				fd;
-	int				read_size;
-	char			buf[BUFFER_SIZE];
+	int				total_size;
 
-	if (!open_file(path, &fd))
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
 		return (invalid_file_data());
 	list = new_linked_list();
-	read_size = read(fd, buf, BUFFER_SIZE);
-	while (read_size > 0)
-	{
-		if (!append_buffer(&list, buf, read_size))
-			return (close(fd), free_linked_list(&list, free),
-				invalid_file_data());
-		read_size = read(fd, buf, BUFFER_SIZE);
-	}
+	total_size = 0;
+	if (!read_to_list(fd, &list, &total_size))
+		return (close(fd), free_linked_list(&list, free), invalid_file_data());
 	close(fd);
-	file_data.data = list_to_char_array(list);
-	file_data.size = list.size;
+	file_data.data = list_to_char_array(list, total_size);
+	file_data.size = total_size;
 	free_linked_list(&list, free);
 	if (!file_data.data)
 		return (invalid_file_data());
